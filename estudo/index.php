69958ef1c24a181e702347c3dee5699403327db4
Guilherme <guilherme@tribomidia.com.br>
2012-07-02 22:34:23 -0300

    Criado o arquivo page.php e linkado as páginas

--- estudo/index.php
+++ estudo/index.php
@@ -7,7 +7,9 @@
 
 <body>
 
+<h1>Vamos começar a usar o Git</h1>
 <p>Vamos apenas imprimir algum texto</p>
+<p><a href="page2.php">Ir para a página 2</a></p>
 
 </body>
 </html>
---
+++ estudo/page2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>ESTUDANDO</title>
+</head>
+
+<body>
+
+<h1>Esta é a página 2</h1>
+<h3>Como está se sentindo?</h3>
+<p>Muito bem, ainda mais usando esta maravilha...</p>
+<p><a href="index.php">Voltar para a Home</a></p>
+
+</body>
+</html>
